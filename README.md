45f0c8e8f2120868965722b720f733681d0a675f
Sunny Lam <sunny.lam@gmail.com>
2018-02-17 14:54:58 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Rapid Backup to Zip
+
+You can use this program to create ZIP file (compressed) snapshots of an entire folder.
+
+You can keep different versions and the filename increments each time it's made.  
+
+--------------------------------
+
+EXAMPLE:  hack_pybook_1.zip, hack_pybook_2.zip ... hack_pybook_n.zip
+
+--------------------------------
+
+The whole point is to automate a very repetitive, dull task.
+
+## References
+
+ABSP:  341
---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+config = {
+	'description': 'Backing up folders as zip archives without doing the work yourself.',
+	'author': 'Sunny Lam',
+	'url': 'URL to get it at',
+	'download_url': 'Where to download it',
+	'author_email': 'sunny.lam@gmail.com',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['NAME'],
+	'scripts': [],
+	'name': 'Rapid Backup to Zip'
+}
+
+setup(**config)
